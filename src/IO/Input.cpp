6169ebae35aa78bce3c9d3a975e6317a7b9c3b5c
Mateo Mađerić <mateo.maderic@gmail.com>
2018-07-25 18:02:33 +0200

    Max value input set to 9999999999,99

--- src/IO/Input.cpp
+++ src/IO/Input.cpp
@@ -228,7 +228,11 @@ int UserInput::_Verify_scrollDown(char& ch) {
 int UserInput::_Verify_value(char& ch) {
 	if (ch == 13)
 		return 3;
-	if (length == 9)
+	else if (length == 10 && ch == 46)
+		return 2;
+	else if ((length == 11 || length == 12) && (ch >= 48 && ch <= 57)) 
+		return 2;
+	else if (length >= 10)
 		return 0;
 	else if (ch == 46) {
 		List* checker = node->previousNode;
