0d26884d36bdc1020fcdc98f7f7c8864bfe1046f
mmaderic <mateo.maderic@maidea.hr>
2018-08-02 11:08:41 +0200

    Latest transactions now can be eddited from dashboard module.

--- src/Modules/Dashboard.cpp
+++ src/Modules/Dashboard.cpp
@@ -45,7 +45,7 @@ void Dashboard::_StartModule() {
 	F2._SetYpos(0);
 	F2._SetAutoDisplay(false);
 	ESC._SetSpecialPrefix("[ESC] ");
-	ESC._SetYpos(0);	
+	ESC._SetYpos(0);
 	ESC._SetAutoDisplay(false);
 	DEL._SetSpecialPrefix("[DEL] ");
 	DEL._SetYpos(0);
@@ -66,7 +66,7 @@ void Dashboard::_StartModule() {
 	//	Make 3 balance frames
 	selector = layout._Select("Balance");
 	int width = selector->_Width() / 3;
-	selector->_Split(unsigned short(1.0 * width / selector->_Width() * 100) , "vertical", "Income", "Temp");
+	selector->_Split(unsigned short(1.0 * width / selector->_Width() * 100), "vertical", "Income", "Temp");
 	selector->_Select("Temp")->_Split(unsigned short(1.0 * width / (selector->_Width() - width) * 100), "vertical", "NetWorth", "Expense");
 
 	//	Get this month stats
@@ -76,7 +76,7 @@ void Dashboard::_StartModule() {
 	for (size_t i = 0; i < TMT.size(); i++) {
 		int ccyID = TMT.at(i)._Currency();
 		TransactionType type = TMT.at(i)._Type();
-		double amount = TMT.at(i)._Amount();	
+		double amount = TMT.at(i)._Amount();
 		if (ccyID != defCCYID)
 			amount = exchangeRateController._ConvertCurrency(amount, ccyID, defCCYID);
 		switch (type) {
@@ -91,7 +91,7 @@ void Dashboard::_StartModule() {
 	}
 
 	//	Calculate net worth
-	double netWorth = 0.0;	
+	double netWorth = 0.0;
 	for (size_t i = 0; i < accountController.accounts->size(); i++) {
 		Account* account = &accountController.accounts->at(i);
 		vector<Account::Balance>* balance = account->_Balance();
@@ -114,7 +114,7 @@ void Dashboard::_StartModule() {
 	Label netWrth(ch, utility::_DoubleToChar(netWorth));
 	Currency* ccy = exchangeRateController._GetCurrency(defCCYID);
 	Label CCY(ccy->_Name());
-	
+
 	//	Total net worth and this month expense and income display
 	selector->_Select("Income")->_AddElements(Label("Income this month", "center"),
 		TextBar("center", Label('+', utility::_DoubleToChar(tmIncome)), CCY));
@@ -122,7 +122,7 @@ void Dashboard::_StartModule() {
 	selector->_Select("NetWorth")->_AddElements(Label("Net Worth", "center"),
 		TextBar("center", netWrth, CCY));
 
-	selector->_Select("Expense")->_AddElements(Label("Expense this month", "center"), 
+	selector->_Select("Expense")->_AddElements(Label("Expense this month", "center"),
 		TextBar("center", Label('-', utility::_DoubleToChar(tmExpense)), CCY));
 
 	//	Latest transactions	
@@ -139,12 +139,13 @@ void Dashboard::_StartModule() {
 
 	vector<Transaction>* LT = transactionController._GetLatestTransactions();
 	Frame* ltFrame = layout._Select("LatestTransactionsBody");
-	ltFrame->_AddLeftPadding(2);	
+	ltFrame->_AddLeftPadding(2);
+	layout._ShowElements();
 
 	//	Latest transaction table - table instance should always be inside main module scope
 	Table table(layout._Select("LatestTransactionsBody"), LT->size(), 6);
 	int minID = transactionController._LastTransactionID(), maxID = 0;
-	if (LT->size() > 0) {		
+	if (LT->size() > 0) {
 		const int  maxAccountLength = 15, maxCategoryLength = 15;
 		int highestAmountLength = 0, highestAccountLength = 0, highestCategoryLength = 0;
 		for (size_t i = 0; i < LT->size(); i++) {
@@ -186,7 +187,7 @@ void Dashboard::_StartModule() {
 		table._SetColumnWidth(2, highestAmountLength + 5);
 		table._SetColumnWidth(3, highestAccountLength + 1);
 		table._SetColumnWidth(4, highestCategoryLength + 1);
-		
+
 		ltFrame->_AddElements(table, ETlabel);
 		layout._Select("Footer")->_AddElements(controlMenu);
 
@@ -199,157 +200,241 @@ void Dashboard::_StartModule() {
 		Label text1("You have no recent transactions.");
 		Label text2("Select [1] to add new transaction record.");
 		text2._SetYpos(2);
-		
+
 		ltFrame->_AddElements(text1, text2);
-	}	
-	layout._ShowElements();
-		
-MainInput: 	
+	}
+
 	//	Main dashboard user input
-	Cursor(2, ::height - 4);
-	UserInput select(InputType::select);
 	int selection = 0;
-	bool selectTransaction = false;
 	while (selection <  1 || selection > mainMenu.size) {
+		if (LT->size() > 0)
+			controlMenu._Show();
+		ltFrame->_ShowElements();		
+		Cursor(2, ::height - 4);
+		UserInput select(InputType::select);
 		select._ReadUserInput();
-		selection = select.selection;		
-		if (select.control == ControlKey::F1 && LT->size() > 0) {	
+		selection = select.selection;
+
+		if (select.control == ControlKey::F1 && LT->size() > 0) {
 			controlMenu._ChangeItem("Select Transaction", "Menu");
-			selectTransaction = true;
 			select._ClearInput();
-			break;
-		}
-		select._ClearInput();
-	}
-	//	Select transaction input
-	if (selectTransaction) {
-SelectTransaction:
-		Cursor(ltFrame->_X1(), ltFrame->_Y2() - 1);
-		UserInput select(InputType::integer);
-		ETlabel._Show();
-		int selection = -1;
-		while (selection <  minID || selection > maxID) {
-			Cursor pos;
-			select._ReadUserInput();
-			selection = select.selection;
-			if (select.control == ControlKey::F1) {
-				controlMenu._ChangeItem("Menu", "Select Transaction");
-				select._ClearInput();
-				ETlabel._Hide();
-				goto MainInput;
-			}
-			select._ClearInput();
-			pos._SetCursorPosition();
-		}
-
-		//	Display selected transaction details
-		latestTransactionTitle._Hide();
-		table._Hide();
-		ETlabel._Hide();
-		selectTransactionTitle._Show();
-
-		F2._SetPadding(F1._Length() + 1);
-		DEL._SetPadding(F1._Length() + 1 + F2._Length() + 1);
-		ESC._SetPadding(F1._Length() + 1 + F2._Length() + 1 + DEL._Length() + 1);
-		F2._Show();	DEL._Show(); ESC._Show();
-
-		Transaction* tr = nullptr;
-		for (size_t i = 0; i < LT->size(); i++) {
-			if (LT->at(i)._ID() == selection)
-				tr = &LT->at(i);
-		}
-
-		Frame tdFrame(*ltFrame->_Select("TransactionDetails"));
-		Currency* cu = exchangeRateController._GetCurrency(tr->_Currency());
-		Account* ac = accountController._GetAccount(tr->_Account());
-		Category* ca = categoryController._GetCategory(tr->_Category());
-		std::vector<const char*>* types = &transactionController.transactionType;
-		int trType = static_cast<int>(tr->_Type());
-
-		TextBar t1(Label("Type:"), Label(types->at(trType)));
-		TextBar t2(Label("Date:"), Label(tr->_Date()));
-		TextBar t3(Label("Amount:"), Label(tr->_AmountChar()));
-		TextBar t4(Label("Currency:"), Label(cu->_Name()));
-		TextBar t5(Label("Account:"), Label(ac->_Name()));
-		TextBar t6(Label("Category:"), Label(ca->_Name()));
-		TextBar t7(Label("Description:"), Label(tr->_Description()));
-
-		tdFrame._AddElements(t1, t2, t3, t4, t5, t6, t7);
-		tdFrame._ShowElements();
-		
-		{	// User input
-		ViewTransaction:
-			Cursor(ltFrame->_X1(), ltFrame->_Y2() - 1);
-			UserInput select(InputType::select);		
+			int selectedID;
 			while (true) {
-				select._ReadUserInput();
-				if (select.control == ControlKey::F1) {					
-					select._ClearInput();
-					controlMenu._ChangeItem("Back", "Cancel");
-					controlMenu._ChangeItem("Menu", "Back");
-					goto MenuInput;
-				}
-				else if (select.control == ControlKey::esc) {
-					F2._Hide();
-					DEL._Hide();
-					ESC._Hide();
-					tdFrame._HideElements();
-					selectTransactionTitle._Hide();
-					latestTransactionTitle._Show();
+				//	Select transaction input
+				Cursor(ltFrame->_X1(), ltFrame->_Y2() - 1);
+				UserInput select(InputType::integer);
+				if(table._Visible() == false)
 					table._Show();
-					goto SelectTransaction;
-				}
-				else if (select.control == ControlKey::F2) {
-					vector<Container> formStore{
-						Container(trType),
-						Container(tr->_Date()),
-						Container(utility::_DoubleToChar(tr->_Amount())),
-						Container(cu->_ID()),
-						Container(ac->_ID()),
-						Container(ca->_ID()),
-						Container(tr->_Description())
-					};
-					Frame teFrame(*ltFrame->_Select("TransactionDetails"));
-					Form editTransaction;
-					editTransaction._SetParentFrame(&tdFrame);
-					editTransaction._AddFields(
-						SelectionField("Type:", transactionController.transactionType, Field::transactionType),
-						DateField("Date:", Field::date),
-						FormField("Amount:", InputType::value, Field::amount),
-						ScrollDown<Currency>("Currency:", *exchangeRateController.currencies, Field::currency),
-						ScrollDown<Account>("Account:", *accountController.accounts, Field::account),
-						ScrollDown_2D<Category>("Category:", *categoryController.categoryList, Field::category),											
-						FormField("Description:", InputType::text, Field::description),
-						ConfirmField("Save?:")
-					);
-					editTransaction._LoadStore(formStore);
-					editTransaction._SetSpecialContentPosition(Form::ContentPos::right);
-					editTransaction._SetSpecialContentFrame(ltFrame->_Select("TDSpec"));
-					editTransaction._Show();
-
-
-				}
-				select._ClearInput();
-			}
-		}	
-		MenuInput:
-		{
-			Cursor(2, ::height - 4);
-			UserInput select(InputType::select);
-			int selection = 0;
-			while (selection <  1 || selection > mainMenu.size) {
-				select._ReadUserInput();
-				selection = select.selection;
-				if (select.control == ControlKey::F1) {
+				ETlabel._Show();
+				int selection = -1;
+				bool mainInput = false;
+				while (selection <  minID || selection > maxID) {
+					Cursor pos;
+					select._ReadUserInput();
+					selection = select.selection;
+					if (select.control == ControlKey::F1) {
+						controlMenu._ChangeItem("Menu", "Select Transaction");
+						select._ClearInput();
+						ETlabel._Hide();
+						mainInput = true;
+						break;
+					}
 					select._ClearInput();
-					controlMenu._ChangeItem("Back", "Menu");
-					controlMenu._ChangeItem("Cancel", "Back");
-					goto ViewTransaction;	
+					pos._SetCursorPosition();
+					selectedID = selection;
 				}
-				select._ClearInput();
-			}
-		}
-	}
+				if (mainInput)
+					break;
+				else {
+					//	Display selected transaction details
+					latestTransactionTitle._Hide();
+					table._Hide();
+					ETlabel._Hide();
+					selectTransactionTitle._Show();
 
-	moduler->_SetNextModule(mainMenu._GetLink(selection), this);
+					F2._SetPadding(F1._Length() + 1);
+					DEL._SetPadding(F1._Length() + 1 + F2._Length() + 1);
+					ESC._SetPadding(F1._Length() + 1 + F2._Length() + 1 + DEL._Length() + 1);
+					F2._Show();	DEL._Show(); ESC._Show();
+
+					Transaction* tr = nullptr;
+					for (size_t i = 0; i < LT->size(); i++) {
+						if (LT->at(i)._ID() == selection)
+							tr = &LT->at(i);
+					}
+
+					Frame tdFrame(*ltFrame->_Select("TransactionDetails"));
+					Currency* cu = exchangeRateController._GetCurrency(tr->_Currency());
+					Account* ac = accountController._GetAccount(tr->_Account());
+					Category* ca = categoryController._GetCategory(tr->_Category());
+					std::vector<const char*>* types = &transactionController.transactionType;
+					int trType = static_cast<int>(tr->_Type());
+
+					TextBar t1(Label("Type:"), Label(types->at(trType)));
+					TextBar t2(Label("Date:"), Label(tr->_Date()));
+					TextBar t3(Label("Amount:"), Label(tr->_AmountChar()));
+					TextBar t4(Label("Currency:"), Label(cu->_Name()));
+					TextBar t5(Label("Account:"), Label(ac->_Name()));
+					TextBar t6(Label("Category:"), Label(ca->_Name()));
+					TextBar t7(Label("Description:"), Label(tr->_Description()));
+
+					tdFrame._AddElements(t1, t2, t3, t4, t5, t6, t7);
+
+					// show transaction details					
+					while (true) {
+						bool cancel = false;
+						tdFrame._ShowElements();
+
+						//	User input
+						Cursor(ltFrame->_X1(), ltFrame->_Y2() - 1);
+						UserInput select(InputType::select);
+						select._ReadUserInput();
+						if (select.control == ControlKey::F1) {
+							select._ClearInput();
+							controlMenu._ChangeItem("Back", "Cancel");
+							controlMenu._ChangeItem("Menu", "Back");
+							// Menu input					
+							while (selection <  1 || selection > mainMenu.size) {
+								Cursor(2, ::height - 4);
+								UserInput select(InputType::select);
+								select._ReadUserInput();
+								selection = select.selection;
+								if (select.control == ControlKey::F1) {
+									controlMenu._ChangeItem("Back", "Menu");
+									controlMenu._ChangeItem("Cancel", "Back");
+									select._ClearInput();
+									break;
+								}	
+								else if (select.control == ControlKey::esc) {
+									cancel = true;
+									break;
+								}
+								select._ClearInput();
+							}
+						}
+						else if (select.control == ControlKey::esc) {
+							F2._Hide();
+							DEL._Hide();
+							ESC._Hide();
+							tdFrame._HideElements();
+							selectTransactionTitle._Hide();
+							latestTransactionTitle._Show();
+							table._Show();
+							break;
+						}
+						else if (select.control == ControlKey::F2) {
+							controlMenu._ChangeItem("Back", "Cancel");
+							F2._Hide();
+							DEL._Hide();
+							ESC._Hide();
+							ESC._SetPadding(F1._Length() + 1);
+							ESC._Show();
+
+							vector<Container> formStore{
+								Container(trType),
+								Container(tr->_Date()),
+								Container(utility::_DoubleToChar(tr->_Amount())),
+								Container(cu->_ID()),
+								Container(ac->_ID()),
+								Container(ca->_ID()),
+								Container(tr->_Description())
+							};
+							Frame teFrame(*ltFrame->_Select("TransactionDetails"));
+							Form editTransaction;
+							editTransaction._SetParentFrame(&tdFrame);
+							editTransaction._AddFields(
+								SelectionField("Type:", transactionController.transactionType, Field::transactionType),
+								DateField("Date:", Field::date),
+								FormField("Amount:", InputType::value, Field::amount),
+								ScrollDown<Currency>("Currency:", *exchangeRateController.currencies, Field::currency),
+								ScrollDown<Account>("Account:", *accountController.accounts, Field::account),
+								ScrollDown_2D<Category>("Category:", *categoryController.categoryList, Field::category),
+								FormField("Description:", InputType::text, Field::description),
+								ConfirmField("Save?:")
+							);
+							editTransaction._LoadStore(formStore);
+							editTransaction._SetSpecialContentPosition(Form::ContentPos::right);
+							editTransaction._SetSpecialContentFrame(ltFrame->_Select("TDSpec"));
+
+							while (true) {
+								// start form 
+								editTransaction._Show();
+								if (editTransaction._Status()) {
+									// if form is completed - update transaction
+									utility::LinkedList<Data*>* data = editTransaction._GetData();
+									transactionController._EditTransaction(data, selectedID, profileController._ActiveProfile()->_ID());
+									cancel = true;
+									break;
+								}
+								else if (editTransaction._IsPaused()) {
+									// if form is paussed (f1 pressed) - menu selection
+									controlMenu._ChangeItem("Menu", "Back");
+									Cursor(2, ::height - 4);
+									UserInput select(InputType::select);
+									int selection = 0;
+									const char* nextModule = nullptr;
+									bool unpause = false;
+									while (selection <  1 || selection > mainMenu.size) {
+										select._ReadUserInput();
+										selection = select.selection;
+										if (select.control == ControlKey::esc) {
+											if (editTransaction._Exit()) {
+												select._ClearInput();
+												cancel = true;
+												controlMenu._ChangeItem("Back", "Menu");
+												controlMenu._ChangeItem("Cancel", "Back");
+												ESC._Hide();
+												break;
+											}
+											else {
+												unpause = true;
+												break;
+											}
+										}
+										else if (select.control == ControlKey::F1) {
+											controlMenu._ChangeItem("Back", "Menu");
+											unpause = true;
+											break;
+										}
+										select._ClearInput();
+									}
+									if (cancel)
+										break;
+									else if (unpause)
+										continue;
+
+									nextModule = mainMenu._GetLink(selection);
+								}
+								else if (!editTransaction._Status()) {
+									// if form status is false - form canceled (esc)
+									controlMenu._ChangeItem("Cancel", "Back");
+									ESC._Hide();
+									cancel = true;
+									break;
+								}
+							}
+							if (cancel) {
+								tdFrame._HideElements();								
+								break;
+							}
+						}
+						if (cancel) {				
+							tdFrame._HideElements();
+							F2._Hide(); DEL._Hide(); ESC._Hide();
+							controlMenu._ChangeItem("Back", "Menu");
+							table._Show();
+							break;
+						}
+					}
+					if (mainInput)
+						break;
+				}
+			}
+			select._ClearInput();
+		}
+		else {
+			moduler->_SetNextModule(mainMenu._GetLink(selection), this);
+			break;
+		}		
+	}
 }
